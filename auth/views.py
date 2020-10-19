62eb0b9cea792a68477e77c08cda7be37c4ae929
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-10-19 12:09:18 +0300

    Initial commit

---
+++ auth/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(
+        request,
+        'home.html'
+    )
+
+
+def signup(request):
+    return render(
+        request,
+        'signup.html'
+    )
+
+
+def login(request):
+    return render(
+        request,
+        'signup.html'
+    )
+
+
+def logout(request):
+    return render(
+        request,
+        'signup.html'
+    )
